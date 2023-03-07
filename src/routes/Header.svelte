dca43a9829f9460c9e5eea0768531cb6731869c3
ElMeGaMerX <127257768+ElMeGaMerX@users.noreply.github.com>
2023-03-07 15:53:38 -0700

    Update Header.svelte

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -23,7 +23,7 @@
 				<a href="/about">About</a>
 			</li>
 			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
-				<a href="/sverdle">Sverdle Inteligente el mejor</a>
+				<a href="/sverdle">Sverdle</a>
 			</li>
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
